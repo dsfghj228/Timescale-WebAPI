4894e492a7dd1218f91fd58620c6e65f823ba85b
anton <antonrumancev67@gmail.com>
2026-01-21 19:14:37 +0300

    Create Db context

---
+++ Backend/Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {}
+    
+    public DbSet<FileImport> Files { get; set; }
+    public DbSet<ResultAggregate> Results { get; set; }
+    public DbSet<ValueEntry> Values { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<FileImport>()
+            .HasMany(f => f.Values)
+            .WithOne(v => v.FileImport)
+            .HasForeignKey(v => v.FileImportId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        modelBuilder.Entity<FileImport>()
+            .HasOne(f => f.Result)
+            .WithOne(r => r.FileImport)
+            .HasForeignKey<ResultAggregate>(r => r.FileImportId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        modelBuilder.Entity<FileImport>()
+            .HasIndex(f => f.FileName).IsUnique();
+        
+        modelBuilder.Entity<ValueEntry>()
+            .HasIndex(v => new { v.FileImportId, v.ExecutionTime });
+        
+        modelBuilder.Entity<ResultAggregate>()
+            .HasIndex(r => r.FirstOperationDate);
+        
+        modelBuilder.Entity<ResultAggregate>()
+            .HasIndex(r => r.AverageValue);
+        
+        modelBuilder.Entity<ResultAggregate>()
+            .HasIndex(r => r.AverageExecutionTime);
+    }
+}
---
+++ Backend/Backend/Migrations/20260121155755_Initial.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    UploadedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TimeDeltaSeconds = table.Column<double>(type: "double precision", nullable: false),
+                    FirstOperationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AverageExecutionTime = table.Column<double>(type: "double precision", nullable: false),
+                    AverageValue = table.Column<double>(type: "double precision", nullable: false),
+                    MedianValue = table.Column<double>(type: "double precision", nullable: false),
+                    MaxValue = table.Column<double>(type: "double precision", nullable: false),
+                    MinValue = table.Column<double>(type: "double precision", nullable: false),
+                    FileImportId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Results_Files_FileImportId",
+                        column: x => x.FileImportId,
+                        principalTable: "Files",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExecutionTime = table.Column<double>(type: "double precision", nullable: false),
+                    Value = table.Column<double>(type: "double precision", nullable: false),
+                    FileImportId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Values_Files_FileImportId",
+                        column: x => x.FileImportId,
+                        principalTable: "Files",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_FileName",
+                table: "Files",
+                column: "FileName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_AverageExecutionTime",
+                table: "Results",
+                column: "AverageExecutionTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_AverageValue",
+                table: "Results",
+                column: "AverageValue");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_FileImportId",
+                table: "Results",
+                column: "FileImportId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_FirstOperationDate",
+                table: "Results",
+                column: "FirstOperationDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Values_FileImportId_ExecutionTime",
+                table: "Values",
+                columns: new[] { "FileImportId", "ExecutionTime" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+
+            migrationBuilder.DropTable(
+                name: "Values");
+
+            migrationBuilder.DropTable(
+                name: "Files");
+        }
+    }
+}
